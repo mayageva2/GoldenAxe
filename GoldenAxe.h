38911309adbc3378b3152e18486e3a2772b455fc
mayageva2 <mayageva7@gmail.com>
2026-05-11 17:23:21 +0300

    moving logic to box2D

--- GoldenAxe.h
+++ GoldenAxe.h
@@ -1,3 +1,8 @@
+#pragma once
+#include <SDL3/SDL.h>
+#include <box2d/box2d.h>
+#include <string>
+#include <iostream>
 #include "bagel.h"
 
 using namespace bagel;
@@ -5,6 +10,19 @@ using namespace bagel;
 namespace GoldenAxe {
 
     // Structs Definitions
+    using Drawable = struct {
+        SDL_FRect part;
+        SDL_FRect dest;
+        float a;
+    };
+
+    struct Animation {
+        int numFrames = 1;
+        int currentFrame = 0;
+        float frameTime = 0.1f;
+        float elapsed = 0.0f;
+    };
+
     struct Position { float x, y; };
     struct Movement { float vx, vy; };
 
@@ -34,28 +52,60 @@ namespace GoldenAxe {
         int goal = 5;
     };
 
+    struct Collider {
+        b2BodyId body;
+    };
+
     // For Score System
     struct EnemyKilledEvent {};
     struct FlaskCollectedEvent {};
 
     // Entities Creation
-    static ent_type CreateHero(float x, float y) {
+    static ent_type CreateHero(b2WorldId world, float x, float y) {
         ent_type hero = World::createEntity();
+
+        b2BodyDef bodyDef = b2DefaultBodyDef();
+        bodyDef.type = b2_dynamicBody;
+        bodyDef.position = {x / 10.0f, y / 10.0f};
+        bodyDef.userData = (void*)(uintptr_t)hero.id;
+        b2BodyId body = b2CreateBody(world, &bodyDef);
+
+        b2Polygon box = b2MakeBox(3.2f, 3.2f);
+        b2ShapeDef shapeDef = b2DefaultShapeDef();
+        b2CreatePolygonShape(body, &shapeDef, &box);
+
         World::addComponent(hero, Position{x, y});
         World::addComponent(hero, Movement{0, 0});
+        World::addComponent(hero, Collider{body});
         World::addComponent(hero, ChangeLives{3, 1});
         World::addComponent(hero, Hit{false, 1});
         World::addComponent(hero, FlaskUsage{0, 5});
         World::addComponent(hero, Score{0});
+        World::addComponent(hero, Drawable{{0,0,64,64}, {x,y,64,64}, 0});
         return hero;
     }
 
-    static ent_type CreateEnemy(float x, float y) {
+    static ent_type CreateEnemy(b2WorldId world, float x, float y, float w, float h, int frames) {
         ent_type enemy = World::createEntity();
+
+        b2BodyDef bodyDef = b2DefaultBodyDef();
+        bodyDef.type = b2_dynamicBody;
+        bodyDef.position = {x / 10.0f, y / 10.0f};
+        bodyDef.userData = (void*)(uintptr_t)enemy.id;
+        b2BodyId body = b2CreateBody(world, &bodyDef);
+
+        b2Polygon box = b2MakeBox((w*1.5f)/20.0f, (h*1.5f)/20.0f);
+        b2ShapeDef shapeDef = b2DefaultShapeDef();
+        b2CreatePolygonShape(body, &shapeDef, &box);
+
         World::addComponent(enemy, Position{x, y});
         World::addComponent(enemy, Movement{0, 0});
+        World::addComponent(enemy, Collider{body});
         World::addComponent(enemy, ChangeLives{1, 0});
         World::addComponent(enemy, AI{true, 1.0f});
+        World::addComponent(enemy, Drawable{{0, 0, w, h}, {x, y, w * 1.5f, h * 1.5f}, 0.0f});
+        World::addComponent(enemy, Animation{frames, 0, 0.15f, 0.0f});
+
         return enemy;
     }
 
@@ -65,6 +115,7 @@ namespace GoldenAxe {
         World::addComponent(santa, Movement{0, 0});
         World::addComponent(santa, ChangeLives{1, 0});
         World::addComponent(santa, AI{true, 2.0f, AIType::RUNNER}); //santa is faster and running from player
+        World::addComponent(santa, Drawable{{0,0,32,32}, {x,y,32,32}, 0});
         return santa;
     }
 
@@ -174,4 +225,83 @@ namespace GoldenAxe {
             }
         }
     };
+
+    class DrawingSystem {
+    public:
+        static void updateAnimation(float deltaTime) {
+            static const Mask mask = MaskBuilder()
+                .set<Animation>()
+                .set<Drawable>()
+                .build();
+
+            for (Entity e = Entity::first(); !e.eof(); e.next()) {
+                if (e.test(mask)) {
+                    auto& anim = e.get<Animation>();
+                    auto& draw = e.get<Drawable>();
+
+                    anim.elapsed += deltaTime;
+                    if (anim.elapsed >= anim.frameTime) {
+                        anim.elapsed = 0.0f;
+                        anim.currentFrame = (anim.currentFrame + 1) % anim.numFrames;
+                        draw.part.x = anim.currentFrame * draw.part.w;
+                    }
+                }
+            }
+        }
+
+        static void draw(SDL_Renderer* ren, SDL_Texture* tex) {
+            static const Mask mask = MaskBuilder().set<Drawable>().set<Position>().build();
+
+            for (Entity e = Entity::first(); !e.eof(); e.next()) {
+                if (e.test(mask)) {
+                    auto& d = e.get<Drawable>();
+                    auto& pos = e.get<Position>();
+
+                    SDL_FRect dest = { pos.x, pos.y, d.dest.w, d.dest.h };
+                    SDL_RenderTextureRotated(ren, tex, &d.part, &dest, d.a, nullptr, SDL_FLIP_NONE);
+                }
+            }
+        }
+    };
+
+    class AISystem {
+    public:
+        static void update(Entity player) {
+            auto& playerPos = player.get<Position>();
+
+            static const Mask aiMask = MaskBuilder()
+                .set<AI>()
+                .set<Position>()
+                .set<Movement>()
+                .build();
+
+            for (Entity e = Entity::first(); !e.eof(); e.next()) {
+                if (e.test(aiMask)) {
+                    auto& ai = e.get<AI>();
+                    auto& pos = e.get<Position>();
+                    auto& mov = e.get<Movement>();
+
+                    float direction = (pos.x < playerPos.x) ? 1.0f : -1.0f;
+
+                    if (ai.type == AIType::CHASER) {
+                        mov.vx = direction * ai.speed;
+                    } else if (ai.type == AIType::RUNNER) {
+                        mov.vx = -direction * ai.speed;
+                    }
+                }
+            }
+        }
+    };
 }
+
+template <> struct bagel::Storage<GoldenAxe::AI> final : bagel::NoInstance {
+    using type = bagel::PackedStorage<GoldenAxe::AI>;
+};
+
+template <> struct bagel::Storage<GoldenAxe::Animation> final : bagel::NoInstance {
+    using type = bagel::PackedStorage<GoldenAxe::Animation>;
+};
+
+template <> struct bagel::Storage<GoldenAxe::Collider> final : bagel::NoInstance {
+    using type = bagel::PackedStorage<GoldenAxe::Collider>;
+};
